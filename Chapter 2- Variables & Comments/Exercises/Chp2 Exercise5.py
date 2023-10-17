e141941200eddc56cebdbecbdd340f4536e13739
SameerMuhammed <144208523+SameerMuhammed@users.noreply.github.com>
2023-10-17 20:57:44 +0400

    Chp2 Exercise5.py

---
+++ Chapter 2- Variables & Comments/Exercises/Chp2 Exercise5.py
@@ -0,0 +1,7 @@
+
+allowance= 50
+usb_cost=6
+total_usb_sticks=allowance//usb_cost
+change=allowance%total_usb_sticks
+print(f"She is able to afford {total_usb_sticks} usb sticks")
+print(f"and her remaining change is {change} POUNDS")
