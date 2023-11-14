036037097d388abb330e5a00949318ace61470cf
SameerMuhammed <144208523+SameerMuhammed@users.noreply.github.com>
2023-11-14 13:58:08 +0400

    Update and rename Exercise5.py to Chp1_Exercise5.py

---
+++ Chapter 1- Getting Started with Python/Exercises/Chp1_Exercise5.py
@@ -0,0 +1,10 @@
+
+# Write a Python program which accepts the radius of a circle from the user and compute the area.
+# user input for the radius
+radius= float(input("Enter the radius of circle"))
+
+# Computing the area of the circle
+area=3.141 * radius** 2
+
+# displaying the computing area
+print(f"The area of a circle with the radius {radius} is {area}")
--- Chapter 1- Getting Started with Python/Exercises/Exercise5.py
+++
@@ -1,6 +0,0 @@
-
-radius= float(input("Enter the radius of circle"))
-
-
-area=3.141 * radius** 2
-print(f"The area of a circle with the radius {radius} is {area}")
