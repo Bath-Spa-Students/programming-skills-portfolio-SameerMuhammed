b08643355f142e6b4750d942c4797b75f5d79ede
SameerMuhammed <144208523+SameerMuhammed@users.noreply.github.com>
2023-11-14 20:56:27 +0400

    Update and rename Chp2 Exercise4.py to Chp2_Exercise4.py

---
+++ Chapter 2- Variables & Comments/Exercises/Chp2_Exercise4.py
@@ -0,0 +1,12 @@
+
+# Use a variable to represent your favorite number. Then,using that variable, create a message that reveals your favorite number. Print
+# that message.
+
+# Assigning a number to a variable
+the_number="619"
+
+# Creating a message revealing the favorite number
+note=f"my favorite number {the_number}"
+
+# print the message
+print(note)
