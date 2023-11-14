376d403f897973f677ae8b61eae8d9cd81b64868
SameerMuhammed <144208523+SameerMuhammed@users.noreply.github.com>
2023-11-14 21:21:35 +0400

    Update and rename Chp3 Exercise1.py to Chp3_Exercise1.py

--- Chapter 3- Data Structures/Exercises/Chp3 Exercise1.py
+++
@@ -1,4 +0,0 @@
-# Store the names of a few of your friends in a list called names. Print each person’s name by accessing each element in the list, one at a time 
-names=["Sam","aditya","hormones","batman","golem"]
-for name in names:
-    print(name)
---
+++ Chapter 3- Data Structures/Exercises/Chp3_Exercise1.py
@@ -0,0 +1,10 @@
+
+# Store the names of a few of your friends in a list called names. Print each person’s name by accessing each element in the list, one at a
+# time.
+
+# list of friends names
+names=["Sam","aditya","hormones","batman","golem"]
+
+# Printing each person's name
+for name in names:
+    print(name)
