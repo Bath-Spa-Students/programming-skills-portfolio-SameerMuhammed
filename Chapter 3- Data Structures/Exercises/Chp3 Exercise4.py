fc56f759672450e75c47a39a7c49c7e729d8bfcb
SameerMuhammed <144208523+SameerMuhammed@users.noreply.github.com>
2023-11-14 21:49:57 +0400

    Update and rename Chp3 Exercise4.py to Chp3_Exercise4.py

--- Chapter 3- Data Structures/Exercises/Chp3 Exercise4.py
+++
@@ -1,5 +0,0 @@
-
-prophets=["Muhammad (saw)","Isa (as)","Ibrahim (as)"]
-for invitations in prophets:
-    idols=f"O prophet {invitations} would love to have u for dinner"
-    print(idols)
---
+++ Chapter 3- Data Structures/Exercises/Chp3_Exercise4.py
@@ -0,0 +1,8 @@
+
+# Guest List
+guest_list=["Muhammad (saw)","Isa (as)","Ibrahim (as)"]
+
+# Inviting each person for dinner
+for invitations in guest_list:
+    idols=f"O prophet {invitations} would love to have u for dinner"
+    print(idols)
