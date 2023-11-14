894ae7bc3b60240c9c535a2fdf4c663d5d42532c
SameerMuhammed <144208523+SameerMuhammed@users.noreply.github.com>
2023-11-14 13:37:13 +0400

    Update Chp1_Exercise3.py

--- Chapter 1- Getting Started with Python/Exercises/Chp1_Exercise3.py
+++ Chapter 1- Getting Started with Python/Exercises/Chp1_Exercise3.py
@@ -1,5 +1,12 @@
 ## Write a Python program to display the current date and time.
 
-date="10/10/2023"
-time="11:06"
-print(date,time)
+# Importing datetime module
+import datetime
+
+# getting current date and time
+current_datetime = datetime.datetime.now()
+
+# formatting and displaying the date and time
+formatted_datetime = current_datetime.strftime("%d/%m/%Y, %H:%M:%S")
+
+print("Current date and time :", formatted_datetime)
