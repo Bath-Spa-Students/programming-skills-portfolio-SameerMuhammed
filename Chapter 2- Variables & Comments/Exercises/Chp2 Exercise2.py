0e49d4559d55d4423a845c80970260e11a2283e9
SameerMuhammed <144208523+SameerMuhammed@users.noreply.github.com>
2023-11-14 20:40:24 +0400

    Update and rename Chp2 Exercise2.py to Chp2_Exercise2.py

--- Chapter 2- Variables & Comments/Exercises/Chp2 Exercise2.py
+++
@@ -1,4 +0,0 @@
-
-quote= "Education is not only the right, but the duty of every muslim, male or female."
-author="Muhammade (SAW)"
-print(f'"{quote}"- {author}')
---
+++ Chapter 2- Variables & Comments/Exercises/Chp2_Exercise2.py
@@ -0,0 +1,11 @@
+
+# Find a quote from a famous person you admire. Print the quote and the name of its author. 
+# Your output should look something like the following, including the quotation marks:
+# Albert Einstein once said, “A person who never made a mistake never tried anything new.
+
+# Assigning message to the variables
+quote= "Education is not only the right, but the duty of every muslim, male or female."
+author="Muhammade (SAW)"
+
+# printing the messages
+print(f'"{quote}"- {author}')
