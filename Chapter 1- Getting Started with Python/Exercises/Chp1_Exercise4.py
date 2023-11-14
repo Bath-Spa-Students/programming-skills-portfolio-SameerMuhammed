d432e0546c9018de3285c68c1e4f8c5e6c132a90
SameerMuhammed <144208523+SameerMuhammed@users.noreply.github.com>
2023-11-14 13:49:37 +0400

    Update and rename Exercise4.py to Chp1_Exercise4.py

---
+++ Chapter 1- Getting Started with Python/Exercises/Chp1_Exercise4.py
@@ -0,0 +1,12 @@
+
+# Write three strings in different variables and print the output as one string.
+
+# Creating three Variable - string1,string2,string3
+string1="hello"
+string2="sup"
+string3="world"
+
+# Concatenating the strings
+total= string1 + string2 + string3
+# Printing the result
+print(total)
