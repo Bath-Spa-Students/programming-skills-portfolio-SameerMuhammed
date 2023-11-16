ebb1d47131676f2d3b9677f8988e6b666a57edb4
SameerMuhammed <144208523+SameerMuhammed@users.noreply.github.com>
2023-11-16 13:00:09 +0400

    Exercise2.py

---
+++ Chapter 7- Functions/Exercises/Exercise2.py
@@ -0,0 +1,6 @@
+# function called favorite_book()
+def favorite_book(title):
+    print(f"One of my favorite books is {title}.")
+
+# Call the function with a book title
+favorite_book("Alice in Wonderland")
