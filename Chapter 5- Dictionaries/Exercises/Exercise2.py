05a81d7ab491083cd077724a7c1afc9a942ca46f
SameerMuhammed <144208523+SameerMuhammed@users.noreply.github.com>
2023-11-16 12:46:22 +0400

    Exercise2.py

---
+++ Chapter 5- Dictionaries/Exercises/Exercise2.py
@@ -0,0 +1,12 @@
+# Glossary dictionary
+programming_glossary = {
+    'variable': 'A named storage location that holds data.',
+    'function': 'A block of organized, reusable code that performs a specific task.',
+    'loop': 'A control flow statement that allows code to be executed repeatedly.',
+    'list': 'An ordered, mutable collection of elements.',
+    'dictionary': 'An unordered, mutable collection of key-value pairs.'
+}
+
+# Print each word and its meaning with a newline between pairs
+for word, meaning in programming_glossary.items():
+    print(f"{word}:\n{meaning}\n")
