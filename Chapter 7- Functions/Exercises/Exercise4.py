08185a109814150341fad0d5c3f414f00f26a62a
SameerMuhammed <144208523+SameerMuhammed@users.noreply.github.com>
2023-11-16 13:14:04 +0400

    Exercise4.py

---
+++ Chapter 7- Functions/Exercises/Exercise4.py
@@ -0,0 +1,13 @@
+# Modify the make_shirt() function
+
+def make_shirt(size="large", message="I love Python"):
+    print(f"Making a {size}-sized shirt with the message: '{message}'.")
+
+# Call the function with default values (large size and default message)
+make_shirt()
+
+# Call the function with medium size and default message
+make_shirt("medium")
+
+# Call the function with a specific size and a different message
+make_shirt(size="small", message="Keep coding!")
