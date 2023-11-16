272042943ff2617d642eff26c72983dffa37fdce
SameerMuhammed <144208523+SameerMuhammed@users.noreply.github.com>
2023-11-16 12:45:34 +0400

    Exercise1.py

--- Chapter 5- Dictionaries/Exercises/Exercise1.py
+++ Chapter 5- Dictionaries/Exercises/Exercise1.py
@@ -1,1 +1,14 @@
 
+# Dictionary storing information about a person
+person_info = {
+    'first_name': 'Sam',
+    'last_name': 'Hashim',
+    'age': 18,
+    'city': 'Moon'
+}
+
+# Printing each piece of information
+print("First Name:", person_info['first_name'])
+print("Last Name:", person_info['last_name'])
+print("Age:", person_info['age'])
+print("City:", person_info['city'])
